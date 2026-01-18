5d71e7a1b1a1af694e19b49d444257cce5236b96
Stephan Wiesenhütter <wiesenhuetter@gmail.com>
2026-01-18 20:24:57 +0100

    handle alarms

--- test_host.py
+++ test_host.py
@@ -1,22 +1,33 @@
 import sys
 import os
-import time
 import logging
+from os import link
+
 from secsgem.common import state_machine
 import secsgem.gem
 import secsgem.hsms
+from secsgem.secs.data_items import ACKC5, ALCD
+from secsgem.secs.variables import U4
+
 from PySide6.QtCore import QObject, Signal
 from PySide6.QtWidgets import (
     QApplication, QMainWindow, QWidget, QVBoxLayout, 
     QLabel, QLineEdit, QPushButton, QHBoxLayout, QTextEdit, QComboBox
 )
+from secsgem.hsms import HsmsMessage
+from secsgem.secs.data_items import ACKC5
+
+secsgem_logger = logging.getLogger("secsgem")
+
 
 class LogHandler(QObject, logging.Handler):
     new_log = Signal(str)
-
-    def __init__(self):
+    def __init__(self, window):
         QObject.__init__(self)
         logging.Handler.__init__(self)
+        self.new_log.connect(window.append_log)
+        self.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+        secsgem_logger.addHandler(self)
 
     def emit(self, record):
         msg = self.format(record)
@@ -84,22 +95,15 @@ class TestHostWindow(QMainWindow):
         self.layout.addWidget(self.log_area)
         
         # Setup Logging
-        self.log_handler = LogHandler()
-        self.log_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
-        self.log_handler.new_log.connect(self.append_log)
-        
-        # Configure secsgem logger to capture all messages
-        secsgem_logger = logging.getLogger("secsgem")
-        secsgem_logger.setLevel(logging.DEBUG)
-        secsgem_logger.addHandler(self.log_handler)
-        
+
         self.settings = secsgem.hsms.HsmsSettings(
             address="127.0.0.1",
             port=5000,
             connect_mode=secsgem.hsms.HsmsConnectMode.ACTIVE,
-            session_id=0
+            session_id=0,
+            t6 = 5  # Wait 5 seconds for a response
         )
-        
+
         self.host = TestHost(self.settings)
         self.enable_host()
         
@@ -164,15 +168,42 @@ class TestHost(secsgem.gem.GemHostHandler):
 
     def __init__(self, settings):
         super().__init__(settings)
+        self.register_stream_function(6, 11, TestHost.event_handler)
 
-    # Override to ignore unhandled event reports
-    def _on_s06f11(self, handler, message):
+    def _on_alarm_received(self, handler, alarm_id, alarm_status, alarm_text):
+        secsgem_logger.info(f"Received event id:{alarm_id.get()} str:{alarm_text.get()} status:{alarm_status.get()}")
+        if alarm_status.get() & ALCD.ALARM_SET:
+            secsgem_logger.info(f"Alarm {alarm_id} is set")
+        else:
+            secsgem_logger.info(f"Alarm {alarm_id} is cleared")
+
+        return ACKC5.ACCEPTED
+
+    @staticmethod
+    def on_connect(event):
+        secsgem_logger.info(f"Connected Handler: {event["connection"].connection_state.current}")
+
+    def event_handler(self, message: HsmsMessage):
+        secsgem_logger.info(f"Received S6F11 Event: {message}")
         """Handle S6F11 (Event Report) - just acknowledge."""
         self.send_response(self.stream_function(6, 12)(0), message.header.system)
         return None
 
+
+    # # Override to ignore unhandled event reports
+    # def _on_s06f11(self, handler, message):
+    #     secsgem_logger.info(f"Received S6F11 Event: {message}")
+    #     """Handle S6F11 (Event Report) - just acknowledge."""
+    #     self.send_response(self.stream_function(6, 12)(0), message.header.system)
+    #     return None
+
+
+
 if __name__ == "__main__":
+    # Configure secsgem logger to capture all messages
+    secsgem_logger.setLevel(logging.DEBUG)
     app = QApplication(sys.argv)
     window = TestHostWindow()
+    log_handler = LogHandler(window)
     window.show()
     sys.exit(app.exec())
